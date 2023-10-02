f6f9800d68484d65081f0d6f701424020c9b0d30
Атамас Мария <atamasmari@yandex.ru>
2023-10-02 18:19:39 +0300

    Initial commit

---
+++ app/src/main/java/com/codingwithme/shoppinglistapp/ShoppingDao.kt
@@ -0,0 +1,22 @@
+package com.codingwithme.shoppinglistapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE) //Анноиация добавления в бд с указанием стратегии замены при уже существующей записи.
+    suspend fun upsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>> //Возвращает лист товаров в реальном времени из БД
+
+}
---
+++ app/src/main/java/com/codingwithme/shoppinglistapp/ShoppingDatabase.kt
@@ -0,0 +1,31 @@
+package com.codingwithme.shoppinglistapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingDatabase: RoomDatabase() {
+
+    abstract fun getShoppingDao(): ShoppingDao
+
+    companion object {
+        @Volatile // аннотация, означающая что права на этот экземпляр будут видны другим потокам
+        private var instance: ShoppingDatabase? = null
+        private val LOCK = Any()
+
+
+        //operator + invoke  означает, что функция выполняется каждый раз, когда мы создаем экземпляр БД наших покупок.
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            //В случае если instance = null, то блок synchronized(LOCK) заблокирует на этот поток, так что никакие другие потоки не смогут вызвать экземпляр БД пока не будет выполнен следующий блок кода.
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, ShoppingDatabase::class.java, "ShoppingDB.db").build()
+    }
+}
